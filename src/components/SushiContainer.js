703eac43f89d8460d3855775b42b1af51ea67d17
jonathan bola <bolajonathan@yahoo.com>
2022-04-19 16:04:08 -0500

    commit tree 9332fe52e201fb07f720f10439d75474e0121bc4

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi.js"
 
-function SushiContainer(props) {
+function SushiContainer({arr}) {
+
+  const[num1, setNum1]=useState(0)
+  const[num2, setNum2]= useState(num1+3)
+  const newArr = arr.filter((el)=>{
+    return arr.indexOf(el)>=num1 && arr.indexOf(el)<=num2 
+  })
+
+  function handleClick(){
+    setNum1(num1+3)
+    setNum2(num2+3)
+
+  }
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      
+      {newArr.map((sushi)=>{
+        return <Sushi key={sushi.id} sushi={sushi}/>
+
+      })}
+      <MoreButton handleClick={handleClick} />
     </div>
   );
 }
